6e8409d4de9804b989b1ccf1697e013287217732
abeachmad <96498781+abeachmad@users.noreply.github.com>
2022-09-26 17:56:13 +0800

    Update abebotinstall.sh

--- abebotinstall.sh
+++ abebotinstall.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 sudo apt update && sudo apt install screen -y
+clear
+
 echo "Welcome to Haqq Node Checker!"
 echo "This is a telegram bot to warn you about your node state"
-echo "to continue, please provide your ID Chat and Validator Address"
+echo "to continue, please provide your telegram ID Chat and Validator Address"
 sleep 1
 
 # set vars
-if [ ! $ID_CHAT ]; then
-read -p "Input Your ID_CHAT: " ID_CHAT
+if [ ! "$ID_CHAT" ]; then
+read -r -p "Input Your ID_CHAT: " ID_CHAT
 echo 'export ID_CHAT='\"${ID_CHAT}\" >> $HOME/.bash_profile
-read -p "Input Your Validator Address: " VALIDATOR_ADDRESS
+read -r -p "Input Your Validator Address: " VALIDATOR_ADDRESS
 echo 'export WALLET_ADDRESS='\"${VALIDATOR_ADDRESS}\" >> $HOME/.bash_profile
 fi
-echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
-. $HOME/.bash_profile
+echo "source $HOME/.bashrc" >> "$HOME"/.bash_profile
+. "$HOME"/.bash_profile
 
 echo -e "Your ID Chat: \e[1m\e[32m${ID_CHAT}\e[0m"
 echo -e "Your Validator Address: \e[1m\e[32m${VALIDATOR_ADDRESS}\e[0m"
@@ -21,5 +23,5 @@ echo '================================================='
 sleep 1
 
 clear
-cd $HOME
+cd "$HOME" || return
 wget -O nodechecker.sh https://raw.githubusercontent.com/abeachmad/abebot/main/nodechecker.sh && chmod +x nodechecker.sh && screen -xR -S abebot ./nodechecker.sh
