8bf1f617b07bfe273fddb83fe74cb915d50c342d
abeachmad <96498781+abeachmad@users.noreply.github.com>
2022-09-26 18:49:18 +0800

    Update nodechecker.sh

--- nodechecker.sh
+++ nodechecker.sh
@@ -13,9 +13,9 @@ elif [ "$jail" == "true" ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ❌ $VALIDATOR IS JAILED"
 fi
 block=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -oj | jq .missed_blocks_counter)
-if  [ "$block" -gt "0" ]; then
+if  [ "$block" -gt 0 ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ⚠️ NODE IS MISSING BLOCKS: $block missed blocks"      
-elif  [ "$block" = "0" ]; then
+elif  [ "$block" -eq 0 ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ NODE MISSED NO BLOCK"
 fi
    printf "sleep"
