326175812e0f6c9c05a94bfe203b4e0f7fcda4bf
abeachmad <96498781+abeachmad@users.noreply.github.com>
2022-09-26 19:20:02 +0800

    Update nodechecker.sh

--- nodechecker.sh
+++ nodechecker.sh
@@ -6,13 +6,13 @@ TOKEN_BOT="5745269620:AAEu6smmKpCJyztqV9sXzx2MNBGsVGVzfTs"
 while true
 do
 cd "$HOME"/ || return
-jail=$(haqqd q staking validator "$VALIDATOR" -oj | jq .jailed)
+jail=$(haqqd q staking validator "$VALIDATOR" -ot | jq .jailed)
 if  [ "$jail" == "false" ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ✅ $VALIDATOR IS FINE"
 elif [ "$jail" == "true" ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ❌ $VALIDATOR IS JAILED"
 fi
-block=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -oj | jq .missed_blocks_counter)
+block=$(haqqd q slashing signing-info $(haqqd tendermint show-validator) -ot | jq .missed_blocks_counter)
 if  [ "$block" != ""0"" ]; then
         curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$ID" -d "parse_mode=html" -d "text= ⚠️ NODE IS MISSING BLOCKS: $block missed blocks"      
 elif  [ "$block" = ""0"" ]; then
